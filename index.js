571d43b0875596eadca9ec32e62e0efea9c61b42
Syed Shayan <shayansyed712@gmail.com>
2024-07-11 13:37:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const navbarOpen = document.getElementById('navbar-open');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+
+navbarOpen.addEventListener('change', () => {
+  if (navbarOpen.checked) {
+    menuIcon.classList.add('hidden');
+    closeIcon.classList.remove('hidden');
+  } else {
+    menuIcon.classList.remove('hidden');
+    closeIcon.classList.add('hidden');
+  }
+});
+
